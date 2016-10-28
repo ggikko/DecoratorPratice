e5722de126e50cf48ae4ddf7a8aee45b0f380c63
ggikko <ggikko@hotmail.com>
2016-10-28 20:56:43 +0900

    add line

--- DecoPraticeApp/app/src/main/java/ggikko/me/decopraticeapp/GgikkoDecoration.java
+++ DecoPraticeApp/app/src/main/java/ggikko/me/decopraticeapp/GgikkoDecoration.java
@@ -1,7 +1,10 @@
 package ggikko.me.decopraticeapp;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -11,9 +14,19 @@ import android.view.View;
 
 public class GgikkoDecoration extends RecyclerView.ItemDecoration {
 
+    private Drawable divider;
+
+    public GgikkoDecoration(Context context) {
+        int[] attr = {android.R.attr.listDivider};
+        final TypedArray a = context.obtainStyledAttributes(attr);
+        divider = a.getDrawable(0);
+        a.recycle();
+    }
+
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
+        outRect.set(0, 0, 0, divider.getIntrinsicHeight());
     }
 
     @Override
